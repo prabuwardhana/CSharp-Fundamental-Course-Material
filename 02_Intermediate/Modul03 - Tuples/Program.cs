43fd20eea3cd4f89833274853fa89fc9baf929e4
Dian Nandiwardhana <prabuwardhana03@yahoo.com>
2021-10-01 23:26:29 +0900

    modify tuple course material

--- 02_Intermediate/Modul03 - Tuples/Program.cs
+++ 02_Intermediate/Modul03 - Tuples/Program.cs
@@ -4,28 +4,28 @@ namespace WorkingWithTuples
 {
     class Program
     {
-        public struct HasilBagi
+        public struct DivResult
         {
-            public double hasilDouble;
-            public int hasilInt;
+            public double DoubleResult;
+            public int IntResult;
         }
 
         static void Main(string[] args)
         {
-            double hasilBagiDouble = Pembagian(20, 3, out int hasilBagiInt);
-            Console.WriteLine("Hasil Bagi: {0}, Hasil konversi ke integer: {1}", hasilBagiDouble, hasilBagiInt);
+            double DivResultDouble = Division(20, 3, out int DivResultInt);
+            Console.WriteLine("Division result: {0}, Result of to integer conversion: {1}", DivResultDouble, DivResultInt);
 
-            HasilBagi hslBagi = Pembagian(20, 3);
-            Console.WriteLine("Hasil Bagi: {0}, Hasil konversi ke integer: {1}", hslBagi.hasilDouble, hslBagi.hasilInt);
+            DivResult divResult = Division(20, 3);
+            Console.WriteLine("Division result: {0}, Result of to integer conversion: {1}", divResult.DoubleResult, divResult.IntResult);
 
-            // (double, int) hslBagiTuple = PembagianTuple(20, 3);
-            // Console.WriteLine("Hasil Bagi: {0}, Hasil konversi ke integer: {1}", hslBagiTuple.Item1, hslBagiTuple.Item2);
+            (double, int) divResultTuple = DivisionReturnsTupleVer2(20, 3);
+            Console.WriteLine("Division result: {0}, Result of to integer conversion: {1}", divResultTuple.Item1, divResultTuple.Item2);
 
-            (double desimal, int bulat) = PembagianTuple(20, 3);
-            Console.WriteLine("Hasil Bagi: {0}, Hasil konversi ke integer: {1}", desimal, bulat);
+            (double resultDouble, int resultInt) = DivisionReturnsTupleVer2(20, 3);
+            Console.WriteLine("Hasil Bagi: {0}, Hasil konversi ke integer: {1}", resultDouble, resultInt);
         }
 
-        static double Pembagian(int x, int y, out int hasilInt)
+        static double Division(int x, int y, out int hasilInt)
         {
             double hasil;
             if (y != 0)
@@ -35,7 +35,7 @@ namespace WorkingWithTuples
             }
             else
             {
-                Console.WriteLine("Bilangan pembagi tidak boleh bernilai 0");
+                Console.WriteLine("Denominator cannot be zero");
                 hasil = 0;
                 hasilInt = 0;
             }
@@ -43,45 +43,45 @@ namespace WorkingWithTuples
             return hasil;
         }
 
-        static HasilBagi Pembagian(int x, int y)
+        static DivResult Division(int x, int y)
         {
-            HasilBagi hsl;
+            DivResult hsl;
 
             if (y != 0)
             {
-                hsl.hasilDouble = (double)x / y;
-                hsl.hasilInt = x / y;
+                hsl.DoubleResult = (double)x / y;
+                hsl.IntResult = x / y;
             }
             else
             {
-                Console.WriteLine("Bilangan pembagi tidak boleh bernilai 0");
-                hsl.hasilDouble = 0;
-                hsl.hasilInt = 0;
+                Console.WriteLine("Denominator cannot be zero");
+                hsl.DoubleResult = 0;
+                hsl.IntResult = 0;
             }
 
             return hsl;
         }
 
-        // static (double, int) PembagianTuple(int x, int y)
-        // {
-        //     (double Desimal, int Bulat) hasil;
+        static (double, int) DivisionReturnsTupleVer1(int x, int y)
+        {
+            (double Desimal, int Bulat) hasil;
 
-        //     if (y!=0)
-        //     {
-        //         hasil.Desimal = (double)x/y;
-        //         hasil.Bulat = x/y;
-        //     }
-        //     else
-        //     {
-        //         Console.WriteLine("Bilangan pembagi tidak boleh bernilai 0");
-        //         hasil.Desimal = 0;
-        //         hasil.Bulat = 0;
-        //     }
+            if (y!=0)
+            {
+                hasil.Desimal = (double)x/y;
+                hasil.Bulat = x/y;
+            }
+            else
+            {
+                Console.WriteLine("Bilangan pembagi tidak boleh bernilai 0");
+                hasil.Desimal = 0;
+                hasil.Bulat = 0;
+            }
 
-        //     return hasil;
-        // }
+            return hasil;
+        }
 
-        static (double, int) PembagianTuple(int x, int y)
+        static (double, int) DivisionReturnsTupleVer2(int x, int y)
         {
             return y != 0 ? ((double)x / y, x / y) : (0, 0);
         }
